d869dcd73fe662822f86fcae53d81b83fbc99d9a
CJ Ratliff <its.carve@gmail.com>
2020-10-26 19:24:56 -0400

    Updated all libraries

--- ui-libraries.php
+++ ui-libraries.php
@@ -29,7 +29,7 @@ class UILibrariesPlugin extends Plugin
             'onPageInitialized' => ['onPageInitialized', 0]
         ]);
     }
-    
+
     public function onPageInitialized()
     {
         // Get a variable from the plugin configuration
@@ -38,21 +38,21 @@ class UILibrariesPlugin extends Plugin
 
         foreach($libraries as $library) {
             if ($library['enabled'] == 'jqueryui')
-                array_push($libs, 'http://code.jquery.com/ui/1.12.1/jquery-ui.min.js','http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+                array_push($libs, 'https://code.jquery.com/jquery-1.12.4.min.js','http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
             else if ($library['enabled'] == 'bootstrap3')
                 array_push($libs, 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css','https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css','https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js');
             else if ($library['enabled'] == 'bootstrap')
-                array_push($libs, 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css','https://code.jquery.com/jquery-3.2.1.slim.min.js','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
+                array_push($libs, 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css','https://code.jquery.com/jquery-3.2.1.slim.min.js','https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js');
             else if ($library['enabled'] == 'uikit')
-                array_push($libs, 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit-core.min.js','https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/css/uikit-core.min.css');
+                array_push($libs, 'https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js','https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit-icons.min.js','https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/css/uikit.min.css');
             else if ($library['enabled'] == 'pure.css')
-                array_push($libs, 'https://unpkg.com/purecss@1.0.0/build/pure-min.css');
+                array_push($libs, 'https://unpkg.com/purecss@2.0.3/build/pure-min.css');
             else if ($library['enabled'] == 'skeleton')
                 array_push($libs, 'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css');
             else if ($library['enabled'] == 'base')
-                array_push($libs, 'https://cdnjs.cloudflare.com/ajax/libs/basscss/8.1.0/css/basscss.min.css');
+                array_push($libs, 'https://unpkg.com/basscss@8.0.2/css/basscss.min.css');
             else if ($library['enabled'] == 'foundation')
-                array_push($libs, 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.3/js/foundation.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.3/css/foundation.min.css');
+                array_push($libs, 'https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js', 'https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css');
             else if ($library['enabled'] == 'materialize')
                 array_push($libs, 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css');
             else if ($library['enabled'] == 'semanticui')
